c14ff6838ab8673d96b7b51db267a66698bf1d85
okazakihh <jofame_29@hotmail.com>
2021-10-25 23:44:11 -0500

    cargue de datos

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Calculadora from "./components/calculadora";
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="tittlec">Calculadora - Pwa</h1>
+      <hr />
+
+      <Calculadora />
+    </div>
+  )
+}
+
+export default App
